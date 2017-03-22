cd432157df7667025707a732b2a21a4e5ed46a93
hanangjie <hanangjie@gmail.com>
2017-03-22 14:34:26 +0800

    de

---
+++ import/a.js
@@ -0,0 +1,20 @@
+{
+	let a = 1;
+	const b = 2;
+	var c = 3;
+	var ff = {
+		ff() {
+			console.log(1);
+		}
+	};
+}
+
+console.log(window.c, window.b, window.a);
+let a = 1;
+{
+	let a = 2;
+	console.log(a);
+}
+
+let input = [1, 2, 3];
+input.map(item => item + 1);

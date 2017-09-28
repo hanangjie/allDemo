931a024265c9378f45fec5d4ffd06b09542a9b34
hanangjie <hanangjie@gmail.com>
2017-09-28 17:42:13 +0800

    d

--- webpack-react/src/index.js
+++ webpack-react/src/index.js
@@ -1,11 +1,22 @@
 import _ from 'lodash';
+import Icon from './icon.jpg';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
+  
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  var myIcon = new Image();
+      myIcon.src = Icon;
+  
+  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!111';
+  btn.onclick = printMe;
 
+  element.appendChild(btn);
   return element;
 }
 
--- webpack-react/webpack.config.js
+++ webpack-react/webpack.config.js
@@ -1,8 +1,33 @@
 var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'   
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),//删除原有的dist
+    new HtmlWebpackPlugin({
+      template: './index.html'
+      })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+           'file-loader'
+         ]
+      }
+    ]
   }
 };
